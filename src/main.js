5756b5e38b0505e8762ba43ac0a163191f4b1d9d
brenoob <breno_1993@outlook.com>
2024-01-22 11:00:57 -0300

    instalação do vuelidate pacote de validação vue

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import App from './App.vue'
 import router from './router'
 
 import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
+import useVuelidate from '@vuelidate/core';
 
+import 'sweetalert2/dist/sweetalert2.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/nprogress/nprogress.css' 
 
 const app = createApp(App)
 
-app.use(router, VueSweetalert2)
+app.use(router, VueSweetalert2, useVuelidate)
 
 app.mount('#app')
